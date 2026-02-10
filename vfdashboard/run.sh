20a85dad1f570562137069a9326fa9bfaf6aaa5b
thaibacgiang <89860738+thaibacgiang@users.noreply.github.com>
2026-02-10 21:09:52 +0700

    Update run.sh

--- vfdashboard/run.sh
+++ vfdashboard/run.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/with-contenv sh
-npm run preview
+#!/usr/bin/with-contenv bash
+set -e
+
+echo "▶ Building VFDashboard..."
+npm install
+npm run build
+
+echo "▶ Starting VFDashboard..."
+npx serve dist -l 8080
