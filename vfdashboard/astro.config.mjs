875627b65efe6f2a8a18a300eeb494410ddec99a
thaibacgiang <89860738+thaibacgiang@users.noreply.github.com>
2026-02-10 21:16:23 +0700

    Update astro.config.mjs

--- vfdashboard/astro.config.mjs
+++ vfdashboard/astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: 'static',
-  site: '/',        // chạy local
-  base: '/vfdashboard/', // nếu dùng /local/vfdashboard
+  output: "static",
+  site: "/",
 });
